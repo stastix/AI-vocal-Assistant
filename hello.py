b4a9df3f6fd71fe729b7d434b8f1b6602658ee7f
stastix <17677479+stastix@users.noreply.github.com>
2023-09-15 20:11:13 +0100

    small update here and there
    note taking youtube api and other new features

--- hello.py
+++ hello.py
@@ -5,7 +5,9 @@ import webbrowser
 import wikipedia
 import wolframalpha
 import keyboard 
-import math 
+import math  
+import pywhatkit as kit 
+import requests 
 
 
 import openai
@@ -13,7 +15,7 @@ import openai
 # Load credentials
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv() 
 
 # Google TTS
 import google.cloud.texttospeech as tts
@@ -24,7 +26,15 @@ import time
 from ctypes import *
 from contextlib import contextmanager
 
-ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p) 
+
+#get weather 
+def get_weather_data(api_key, city):
+    base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(base_url)
+    weather_data = response.json()
+    return weather_data
+
 
 def py_error_handler(filename, line, function, err, fmt):
     pass
@@ -49,7 +59,7 @@ tts_type = 'local' # google or local
 # Local speech engine initialisation
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id) # 0 = male, 1 = female
+engine.setProperty('voice', voices[1].id) # 0 = male, 1 = female
 
 # Google TTS client
 def google_text_to_wav(voice_name: str, text: str):
@@ -75,19 +85,19 @@ def google_text_to_wav(voice_name: str, text: str):
 
 # Configure browser
 # Set the path
-firefox_path = r"usr/bin/firefox"
+chrome_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
 # Register the browser
-webbrowser.register('firefox', None, 
-                    webbrowser.BackgroundBrowser(firefox_path))
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 
 # Wolfram Alpha client
-appId = '5R49J7-J888YX9J2V'
+appId = 'E97784-2U55VT546J' 
 wolframClient = wolframalpha.Client(appId)
 
-def speak(text, rate = 120):
+def speak(text, rate = 120): 
     time.sleep(0.3)
-    try:     
-        if tts_type == 'local':
+    try:      
+        if tts_type == 'local': 
+            
             engine.setProperty('rate', rate) 
             engine.say(text, 'txt')
             engine.runAndWait()
@@ -106,7 +116,9 @@ def speak(text, rate = 120):
                 pygame.mixer.quit()
         except:
             pass
-        return
+        return 
+        
+            
 
 
 def parseCommand():
@@ -120,14 +132,15 @@ def parseCommand():
 
         try:
             print('Recognizing speech...')
-            query = listener.recognize_google(input_speech, language='en_gb')
+            query = listener.recognize_google(input_speech, language='en_gb') 
             print(f'The input speech was: {query}')
+            
 
         except Exception as exception:
             print('I did not quite catch that')
             print(exception)
 
-            return 'None'
+            return None;
 
         return query
 
@@ -177,85 +190,142 @@ def search_wolframalpha(keyword=''):
             # Could search wiki instead here? 
             return question
 
-def query_openai(prompt = ""):
-    openai.organization = os.environ['OPENAI_ORG']
-    openai.api_key = os.environ['OPENAI_API_KEY']
+def query_openai(prompt="",model="GPT-3"):
+    # Set your OpenAI API key 
+    api_key=os.environ.get("OPENAI_API_KEY")
+    openai.api_key = api_key
 
-    # Temperature is a measure of randonmess
+    
+
+    # Set the API key for authentication
+
+    # Temperature is a measure of randomness
     # Max_tokens is the number of tokens to generate
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine=model,
         prompt=prompt,
-        temperature=0.3,
-        max_tokens=80,
-
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
+    return response.choices[0].text.strip()  # Return the generated text
 
-    return response.choices[0].text
 
 # Main loop
 if __name__ == '__main__': 
     speak('All systems nominal.', 120)
+    query=''
+    count=0
 
-    while True:
+    while True: 
+        count+=1 
         # Parse as a list
-        # query = 'computer say hello'.split()
-        query = parseCommand().lower().split()
+        # query = 'computer say hello'.split() 
+        try:
+            query = parseCommand().lower().split()  
+        except: 
+            speak('goodbye')
+            break 
+        
 
         if query[0] in activationWords and len(query) > 1:
             query.pop(0)
 
             # Set commands
-            if query[0] == 'say':
-                if 'hello' in query:
-                    speak('Greetings, all!')
-                else:
-                    query.pop(0) # Remove 'say'
-                    speech = ' '.join(query) 
-                    speak(speech)
-
-            # Query OpenAI
-            if query[0] == 'insight':
-                query.pop(0) # Remove 'insight'
-                query = ' '.join(query)
-                speech = query_openai(query)
-                speak("Ok")
+        if query[0] == 'say':
+            if 'hello' in query:
+                speak('Greetings, all!')
+            else:
+                query.pop(0) # Remove 'say'
+                speech = ' '.join(query) 
                 speak(speech)
 
+            # Query OpenAI
+        if query[0] == 'insight' or query[0] =='inside' or 'chatgpt' in query or 'gpt' in query:
+            query.pop(0) # Remove 'insight'  
+            if 'chatgpt' in query:
+                query.remove('chatgpt') 
+            elif 'gpt' in query:
+                query.remove('gpt')
+            query = ' '.join(query) 
+            try:
+                speech = query_openai(query)
+                speak("Ok")
+                speak(speech) 
+            except: 
+                speak("plz set your OPENAI KEY variable ")
+
+
             # Navigation
-            if query[0] == 'go' and query[1] == 'to':
-                speak('Opening... ')
-                # Assume the structure is activation word + go to, so let's remove the next two words
-                query = ' '.join(query[2:])
-                webbrowser.get('chrome').open_new(query)
+        if query[0] == 'go' and query[1] == 'to' or query[0]=='search' and query[1]=='for' or query[0]=='open' :
+            speak('Opening... ')
+            # Assume the structure is activation word + go to, so let's remove the next two words
+            query = ' '.join(query[2:])
+            try:
+                 webbrowser.open(query)
+            except webbrowser.Error as e:
+                print("Web browser error:", e)
+            except Exception as e:
+                print("An error occurred:", e)
+
 
             # Wikipedia
-            if query[0] == 'wikipedia':
-                query = ' '.join(query[1:])
-                speak('Querying the universal databank')
-                time.sleep(2)
-                speak(search_wikipedia(query))
+        if query[0] == 'wikipedia':
+            
+            query = ' '.join(query[1:])
+            speak('Querying the universal databank')
+            time.sleep(2) 
+            
+            speak(search_wikipedia(query),200) 
+            engine.runAndWait()
+ 
+                
+            
 
             # Wolfram Alpha
-            if query[0] == 'compute' or query[0] == 'computer':
-                query = ' '.join(query[1:])
-                try:
-                    result = search_wolframalpha(query)
-                    speak(result)
-                except:
-                    speak('Unable to compute')
+        if query[0] == 'compute' or query[0] == 'computer':
+            query = ' '.join(query[1:])
+            try:
+                result = search_wolframalpha(query)
+                speak(result)
+            except:
+                speak('Unable to compute')
 
             # Note taking
-            if query[0] == 'log':
-                speak('Ready to record your note')
-                newNote = parseCommand().lower()
-                now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-                with open('note_%s.txt' % now, 'w') as newFile:
-                    newFile.write(now)
-                    newFile.write(' ')
-                    newFile.write(newNote)
+        if query[0] == 'log' or query[0]=='note' or query[0] == 'write' or query[0]=='right':  
+            speak('Ready to record your note')
+            newNote = parseCommand().lower()
+            now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            with open('note_%s.txt' % now, 'w') as newFile:
+                newFile.write(now)
+                newFile.write(' ')
+                newFile.write(newNote)
                 speak('Note written')
-
-            if query[0] == 'exit':
-                speak('Goodbye')
-                break
+        if query[0]=='are' and query[1]=='you': 
+            speak('cannot answer that' )  
+        
+        
+        if query[0]=='play' and 'youtube' in query : 
+            query.pop(0) 
+            query.remove('youtube')
+            kit.playonyt(query)
+            
+        if query[0]=='search' and 'youtube' in query: 
+            query.pop(0) 
+            if query[0]=='for':
+                query.pop(0)
+            if 'on youtube' in query: 
+                query.remove('on youtube')
+            else:
+                query.remove('youtube') 
+            kit.search(' '.join(query))               
+        if query[0] == 'exit':
+            speak('Goodbye') 
+            break 
+        if 'your' and 'name' in query: 
+            speak('My name is Assistant')    
+            
+            
+        
+             
